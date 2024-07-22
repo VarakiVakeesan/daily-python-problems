9e62871884fdc4253424ac7f9aa3d168e09449d5
varakiVakeesan <varakivakeesan23@gmail.com>
2024-07-22 23:24:23 +0530

    Problem 1 - Solved - Basic Idea

---
+++ problem-2024-07-22/Solution1.py
@@ -0,0 +1,11 @@
+#Given a positive integer N, determine whether it is odd or even.
+
+def check_odd_or_even(N) :
+    if N % 2 == 0:
+        print(f"{N} is Even Number")
+    else:
+        print(f"{N} is Odd Number")
+
+
+num = int(input('Enter a Positive Interger : '))
+check_odd_or_even(num)
